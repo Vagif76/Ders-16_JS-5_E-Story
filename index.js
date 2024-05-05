d091b3c91b228948a05f5119f8ccfd021cd20156
vagif76 <vagif.ahmadov76@gmail.com>
2024-05-05 23:01:18 +0400

    border

--- index.js
+++ index.js
@@ -17,18 +17,20 @@ async function getMelumatlariGetir() {
         for(let i=0; i< gelenMehsullar.length; i++) {
             mehsullarinYeri.innerHTML += `
             <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card my-2 border-0">
-                        <img src="${gelenMehsullar[i].image}" class="card-img-top" alt="...">
-                        <div class="card-body d-flex flex-column justify-content-evenly">
-                          <h5 class="card-title text-center">${gelenMehsullar[i].title.slice(0, 20)}...</h5>
-                          <p class="card-text">${gelenMehsullar[i].description.slice(0, 40)}...</p>
-                          <strong>Price: ${gelenMehsullar[i].price} USD</strong>
-                          <br>
-                          <br>
-                          <a href="#" class="btn btn-primary">Read more</a>
-                        </div>
-                      </div>
-                </div>`
+                <div class="card my-2 p-2 border-primary-subtle">
+                    <img src="${gelenMehsullar[i].image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h3 class="text-center text-danger">${gelenMehsullar[i].category}</h3>
+                      <h5 class="card-title">${gelenMehsullar[i].title.slice(0, 20)}...</h5>
+                      <p class="card-text">${gelenMehsullar[i].description.slice(0, 35)}...</p>
+                      <strong>Price: ${gelenMehsullar[i].price} USD </strong>
+                      <br>
+                      <br>
+                      <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            `
         }
     }
 
